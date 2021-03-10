a52b1b0f55e36439c7f0db60d8a44e02dd2a4dda
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2021-03-11 08:45:06 +0900

    updated route_table

--- vpc.tf
+++ vpc.tf
@@ -26,6 +26,11 @@ resource "aws_internet_gateway" "training" {
 resource "aws_route_table" "training" {
   vpc_id = aws_vpc.training.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.training.id
+  }
+
   tags = {
     Name = "public_route_table"
   }
