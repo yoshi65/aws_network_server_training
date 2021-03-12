c66463134f7c7b5e4e0e12b871e4803a3462eea8
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2021-03-12 11:13:39 +0900

    added ec2 and security_group

---
+++ ec2.tf
@@ -0,0 +1,35 @@
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t2.micro"
+
+  subnet_id                   = aws_subnet.training.id
+  associate_public_ip_address = true
+  private_ip                  = "10.0.1.10"
+  security_groups             = [aws_security_group.WEB-SG.id]
+  key_name                    = aws_key_pair.training.key_name
+
+  tags = {
+    Name = "web_server"
+  }
+}
+
+resource "aws_key_pair" "training" {
+  key_name   = "training"
+  public_key = file("~/.ssh/training.pub")
+}
---
+++ security_group.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "WEB-SG" {
+  name        = "WEB-SG"
+  description = "Created for training"
+  vpc_id      = aws_vpc.training.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
