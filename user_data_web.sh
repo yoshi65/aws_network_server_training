7a75a7043c1a6e5e6fab247a3974f14b55188b4e
Yoshitaka Yamazaki <yoshi65.ymzk@gmail.com>
2021-03-19 10:19:06 +0900

    updated user_data for web

--- user_data_web.sh
+++ user_data_web.sh
@@ -4,3 +4,16 @@ sudo yum update
 sudo yum -y install httpd
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
+
+# 8-3
+sudo amazon-linux-extras -y install php7.3
+sudo yum -y install php php-mbstring
+sudo yum -y install mariadb-server
+
+cd ~
+wget https://ja.wordpress.org/latest-ja.tar.gz
+tar xzvf latest-ja.tar.gz
+
+cd wordpress
+sudo cp -r * /var/www/html
+sudo chown apache:apache /var/www/html -R
